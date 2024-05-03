622a93354f52a776a2d3288da25206781df5a701
Aakash Rathod <aakash331998@gmail.com>
2024-05-02 20:05:34 -0400

    Update MaxTwinSumLinkedList.sql

--- MaxTwinSumLinkedList.sql
+++ MaxTwinSumLinkedList.sql
@@ -18,23 +18,23 @@ class Solution:
 
         return max_tot
 
-# class Solution(object):
-#     def pairSum(self, head):
-#         current = head
-#         values = []
+class Solution(object):
+    def pairSum(self, head):
+        current = head
+        values = []
 
-#         # while current:
-#         #     values.append(current.val)
-#         #     current = current.next
+        while current:
+            values.append(current.val)
+            current = current.next
         
-#         # i = 0
-#         # j = len(values) - 1
-#         # maximumSum = 0
-#         # while(i < j):
-#         #     maximumSum = max(maximumSum, values[i] + values[j])
-#         #     i = i + 1
-#         #     j = j - 1
+        i = 0
+        j = len(values) - 1
+        maximumSum = 0
+        while(i < j):
+            maximumSum = max(maximumSum, values[i] + values[j])
+            i = i + 1
+            j = j - 1
         
-#         # return maximumSum
+        return maximumSum
 
 
