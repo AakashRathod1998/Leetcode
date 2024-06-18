24f9b87ccc3f67853bc86e432bc105002828d6ea
Aakash Rathod <aakash331998@gmail.com>
2024-06-18 19:31:44 -0400

    Update EvaluateReversePolishNotation.py

--- EvaluateReversePolishNotation.py
+++ EvaluateReversePolishNotation.py
@@ -1,3 +1,28 @@
+# Approach 1
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        
+        stack = []
+
+        for i in tokens:
+            
+            if i not in '+-*/':
+                stack.append(int(i))
+            else:
+                num1 = stack.pop(-1)
+                num2 = stack.pop(-1)
+
+                if i == '+':
+                    stack.append(num2+num1)
+                elif i == '-':
+                    stack.append(num2-num1)
+                elif i == '/':
+                    stack.append(int(num2/num1))
+                elif i == '*':
+                    stack.append(num2*num1)
+        return stack[0]
+
+# Approach 2
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
 
